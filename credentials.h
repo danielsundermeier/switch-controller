7b54e449c4d55087c577faadf14b96530becb30d
Daniel Sundermeier <daniel@hof-sundermeier.de>
2018-09-04 03:49:23 +0200

    Example Credentials file added

---
+++ credentials.h
@@ -0,0 +1,9 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Replace with your actual SSID and password:
+#define MQTT_PASSWORD "MQTT password here"
+#define MQTT_SERVER "MQTT IP-Adress here"
+#define MQTT_USER "MQTT User here"
+
+#endif
